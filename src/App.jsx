bd88b8c09fb630e729751ba4464a9134a82ce595
heba <habdelhadi08@gmail.com>
2024-08-17 13:12:25 -0400

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import{Route, Routes} from "react-router-dom"
+import Currencies from './pages/Currencies'
+import Price from './pages/Price'
+import Main from './pages/Main'
+import Nav from './components/Nav'
+
+export default function App() {
+
+
+  return (
+    <div className="App">
+      <Nav/>
+    <Routes>
+    <Route path="/" element={<Main/>}/>
+    <Route path="/currencies" element={<Currencies/>}/>
+     <Route path="/price/:symbol" element={<Price/>}/>
+     </Routes>
+     </div>
+  );
+}
+
+
